a98f3032d00937a8a095abb9c8c981282f9b38a1
aben20807 <aben20807@gmail.com>
2021-12-29 13:46:02 +0800

    Use subprocess

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,18 @@
-import os
 from pathlib import Path
+from typing import Tuple
 import pytest
+import subprocess
+
+
+def exec_command(cmd: str) -> Tuple[str, str, int]:
+    process = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        shell=True,
+    )
+    return process.stdout, process.stderr, process.returncode
 
 
 @pytest.fixture
@@ -9,26 +21,23 @@ def base_path() -> Path:
     return Path(__file__).parent.parent
 
 
-def test_judge_correct(base_path: Path, monkeypatch: pytest.MonkeyPatch, capfd):
+def test_judge_correct(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     monkeypatch.chdir(base_path / "examples" / "judge" / "correct")
-    returncode = os.system("judge")
-    out, _ = capfd.readouterr()
+    out, _, returncode = exec_command("judge")
     assert returncode == 0
     assert "100/100" in out
 
 
-def test_judge_wrong(base_path: Path, monkeypatch: pytest.MonkeyPatch, capfd):
+def test_judge_wrong(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     monkeypatch.chdir(base_path / "examples" / "judge" / "wrong")
-    returncode = os.system("judge")
-    out, _ = capfd.readouterr()
+    out, _, returncode = exec_command("judge")
     assert returncode != 0
     assert "90/100" in out
 
 
-def test_ta_judge(base_path: Path, monkeypatch: pytest.MonkeyPatch, capfd):
+def test_ta_judge(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     monkeypatch.chdir(base_path / "examples" / "ta_judge")
-    returncode = os.system("ta_judge -j 1")
-    out, _ = capfd.readouterr()
+    out, _, returncode = exec_command("ta_judge")
     assert returncode == 0
     assert "OU2345678" in out
     assert "F12345678" in out
